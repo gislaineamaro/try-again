2e406e3f250b997f8b5a68e1b1629e8156db2a93
Gisa <gislaineamaro013@gmail.com>
2024-07-28 16:31:33 -0300

    chore: update lint config with prettier

---
+++ eslint.config.js
@@ -0,0 +1,80 @@
+import typescriptEslintPlugin from '@typescript-eslint/eslint-plugin';
+import typescriptEslintParser from '@typescript-eslint/parser';
+import eslintPluginReact from 'eslint-plugin-react';
+import eslintPluginReactHooks from 'eslint-plugin-react-hooks';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+
+export default [
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules', 'public', 'react-app-env.d.ts', 'yarn.lock'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: typescriptEslintParser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      'prefer-const': 'warn',
+      'no-unused-vars': 'warn',
+      'object-shorthand': 'warn',
+      '@typescript-eslint/array-type': [
+        'warn',
+        {
+          default: 'array',
+        },
+      ],
+      '@typescript-eslint/consistent-type-assertions': [
+        'warn',
+        {
+          assertionStyle: 'as',
+          objectLiteralTypeAssertions: 'never',
+        },
+      ],
+      'react/jsx-fragments': ['warn', 'syntax'],
+      'react/jsx-filename-extension': [
+        'warn',
+        {
+          extensions: ['ts', 'tsx'],
+        },
+      ],
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'prettier/prettier': 'warn',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': typescriptEslintPlugin,
+      react: eslintPluginReact,
+      'react-hooks': eslintPluginReactHooks,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...typescriptEslintPlugin.configs.recommended.rules,
+      ...eslintPluginReact.configs.recommended.rules,
+      ...eslintPluginReactHooks.configs.recommended.rules,
+      ...eslintPluginPrettier.configs.recommended.rules,
+    },
+  },
+];
